fba8000d6fd8b43ed010f64cec8cf2dd4b388b45
Lian <lianlongfeng@gmail.com>
2022-07-18 13:20:37 -0400

    solution

--- src/count-context/index.tsx
+++ src/count-context/index.tsx
@@ -1,40 +1,64 @@
 import {
-  Context,
   createContext,
-  Dispatch,
   FC,
   ReactElement,
-  SetStateAction,
   useContext,
   useState,
+  useEffect,
+  useCallback,
 } from "react";
 
-declare global {
-  interface Window {
-    _CountContext_: Context<CountContextState>;
-  }
-}
+type CountContextState = [number, () => void];
 
-type CountContextState = [number, Dispatch<SetStateAction<number>>];
+type Listener = () => void;
 
-// const getContext = () => {
-//   window._CountContext_ =
-//     window._CountContext_ || createContext<CountContextState>([0, () => null]);
-//   return window._CountContext_;
+// const createExternalStore = <S,>(initialState: S) => {
+//   let state = initialState;
+//   const getState = () => state;
+//   const listeners = new Set<Listener>();
+//   const setState = (fn: (s: S) => S) => {
+//     state = fn(state);
+//     listeners.forEach((l) => l());
+//   };
+//   const subscribe = (listener: Listener) => {
+//     listeners.add(listener);
+//     return () => {
+//       listeners.delete(listener);
+//     };
+//   };
+//   return { getState, setState, subscribe };
 // };
 
-// const CountContext = getContext();
+// const store = createExternalStore({ count: 0 });
+
 const CountContext = createContext<CountContextState>([0, () => null]);
 
 export const CountProvider: FC<{ children: ReactElement }> = ({ children }) => {
   const [count, setCount] = useState(0);
 
+  const inc = () => setCount((prev) => prev + 1);
+
   return (
-    <CountContext.Provider value={[count, setCount]}>
+    <CountContext.Provider value={[count, inc]}>
       {children}
     </CountContext.Provider>
   );
 };
 
-// export const useCount = () => useContext(getContext());
 export const useCount = () => useContext(CountContext);
+
+// export const useCount = (): [number, () => void] => {
+//   const [count, setCount] = useState(() => store.getState().count);
+//   useEffect(() => {
+//     const callback = () => setCount(store.getState().count);
+//     const unsubscribe = store.subscribe(callback);
+//     callback();
+//     return unsubscribe;
+//   }, []);
+
+//   const inc = useCallback(() => {
+//     store.setState((prev) => ({ count: prev.count + 1 }));
+//   }, []);
+
+//   return [count, inc];
+// };
